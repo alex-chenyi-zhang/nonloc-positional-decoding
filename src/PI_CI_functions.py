da48c496989847ab93e12136c658fd85b5f8d547
Alex Chen Yi Zhang <alex@MacBook-Pro-di-Alex.local>
2025-03-12 21:18:08 +0100

    Fixed mistake in function to sample from gaussian process and created notebook with example usage

--- src/PI_CI_functions.py
+++ src/PI_CI_functions.py
@@ -16,42 +16,47 @@ def exponential_mean_function(x_positions, lamb):
     """
     return np.exp(-x_positions / lamb)
 
-def sample_from_gp(x_positions, mean_function, covariance_kernel, corr_length, delta_x, sigma_S, sigma_int, mu_S):
+def sample_from_gp(x_positions, covariance_kernel, corr_length, delta_x, sigma_ext, sigma_int, mu_S, lamb):
     """
     Samples from a Gaussian Process with the given mean function and covariance kernel with varying correlation length 
-    scale. Then a spatially independent noise of amplitude sigma_int is added to the samples.
+    scale. Then a spatially independent noise of amplitude sigma_int is added to the samples. 
+    (For now we just use expoenential decay as mean function)
 
     Parameters:
     x_positions (2D array): Vector of x positions.
-    mean_function (callable): Mean function of the Gaussian Process.
     covariance_kernel (string): Type of kernel to use in the Gaussian Process.
     corr_length (float): Correlation length scale of the covariance kernel, expressed in number of neighbors.
     delta_x (float): Typical spacing between cells
     sigma_int (float): Standard deviation of the spatially independent component of the noise.
-    sigma_S (float): Standard deviation of the spatially correlated noise.
+    sigma_ext (float): Standard deviation of the spatially correlated noise.
     mu_S (float): Mean value scaling factor.
+    lamb (float): Decay constant of the exponential function
 
     Returns:
     np.ndarray: Samples from the Gaussian Process.
     """
     n_embryos = x_positions.shape[0]
     n_cells = x_positions.shape[1]
-    
+    mean_function = exponential_mean_function
     # Compute the mean values evaluated at the x_positions
-    mean_values = mean_function(x_positions)
+    mean_values = mean_function(x_positions, lamb)
     
-    if covariance_kernel == 'Squared Exponential':
-        kernel = sigma_S**2 * RBF(length_scale=corr_length)
-    elif covariance_kernel == 'Simple Exponential':
-        kernel = sigma_S**2 * Matern(length_scale=corr_length, nu=0.5)
+    if covariance_kernel == 'SquaredExponential':
+        kernel = sigma_ext**2 * RBF(length_scale=corr_length * delta_x)
+    elif covariance_kernel == 'SimpleExponential':
+        kernel = sigma_ext**2 * Matern(length_scale=corr_length * delta_x, nu=0.5)
     else:
         raise ValueError('Invalid covariance kernel. Supported kernels are: "Squared Exponential" and "Simple Exponential"')
 
     # Create a Gaussian process regressor with the selected kernel
     gp = GaussianProcessRegressor(kernel=kernel, alpha=0)
 
+    samples = np.zeros((n_embryos, n_cells))
     # Sample from the Gaussian Process, i.e. the spatially correlated noise
-    samples = gp.sample_y(x_positions, n_samples=n_embryos)
+    for i_embryo in range(n_embryos):
+        seed = np.random.randint(0, 2**32 - 1)
+        samples[i_embryo,:] = gp.sample_y(x_positions[i_embryo].reshape(-1, 1), random_state=seed).flatten()
+
 
     # Rescale the fluctuations by the mean function
     samples *= mean_values
@@ -62,4 +67,4 @@ def sample_from_gp(x_positions, mean_function, covariance_kernel, corr_length, d
     else:    
         samples_with_mean = mu_S * mean_values + samples
 
-    return samples_with_mean.T
+    return samples_with_mean
